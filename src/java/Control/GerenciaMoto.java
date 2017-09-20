d9854e1b87eeae78cb746bcbe8b7e734ffad5a9d
adaog <adaog@172.16.1.4>
2017-09-20 08:20:37 -0300

    att oneServlet

--- src/java/Control/GerenciaMoto.java
+++ src/java/Control/GerenciaMoto.java
@@ -9,8 +9,6 @@ import Dao.MotoDao;
 import Model.Moto;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,36 +39,30 @@ public class GerenciaMoto extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-        System.out.println("Chegou na servlet");
+        //System.out.println("Chegou na servlet");
 
         String acao = request.getParameter("acao"); //busca o value do botao clicado
-        int id_moto = Integer.valueOf(request.getParameter("id_moto"));
+        
 
         if (acao.equals("Excluir")) {
+            int id_moto = Integer.valueOf(request.getParameter("id_moto"));
             System.out.println("você clicou no botao excluir");
             System.out.println("id da moto: " + id_moto);
-            
+
             Moto m = new Moto();
             m.setId(id_moto);
-            
+
             try {
                 MotoDao dao = new MotoDao();
                 dao.remove(m);
             } catch (ClassNotFoundException ex) {
-                System.out.println("Erro ao Excluir moto servlet "+ ex.getMessage());
+                System.out.println("Erro ao Excluir moto servlet " + ex.getMessage());
             }
-              //redirecionamento automatico 
+            //redirecionamento automatico 
             RequestDispatcher rd = request.getRequestDispatcher("/consulta.jsp");
 
             rd.forward(request, response);
-            
-            
-        } else if (acao.equals("Editar")) {
-           //Fazer função de preencher objeto por id na Dao
-           //retornar esse objeto na servlet
-           //passar os parametros do objeto para a jsp alterar
-            
-        
+
         } else if (acao.equals("Cadastrar")) {
             String modelo = request.getParameter("modelo");
             String marca = request.getParameter("marca");
@@ -97,11 +89,49 @@ public class GerenciaMoto extends HttpServlet {
             RequestDispatcher rd = request.getRequestDispatcher("/consulta.jsp");
 
             rd.forward(request, response);
-        }
+        } else if (acao.equals("Editar")) {
+            int id_moto = Integer.valueOf(request.getParameter("id_moto"));
+            String modelo = request.getParameter("modelo");
+            String marca = request.getParameter("marca");
+            String potencia = request.getParameter("potencia");
+            String ano = request.getParameter("ano");
+            String valor = request.getParameter("valor");
+            
+            PrintWriter out = response.getWriter();
 
-        /* 
-       
-         */
+            out.println("<html>");
+            out.println("<body>");
+            out.println("   <form action=\"GerenciaMoto\" method=\"post\">\n" +
+"            <table>\n" +
+"                <tr>   \n" +
+"                    <td> ID:</td> <td>  <input type=\"text\" name=\"id\" readonly=\"true\" value="+id_moto +"  /></td>\n" +
+"                </tr>\n" +
+"                <tr>   \n" +
+"                    <td> Marca:</td> <td>  <input type=\"text\" name=\"marca\" value=\"marca\" /></td>\n" +
+"                </tr>\n" +
+"                <tr> \n" +
+"                    <td> Modelo:</td> <td> <input type=\"text\" name=\"modelo\" /></td>\n" +
+"                </tr>\n" +
+"                 <tr>\n" +
+"                    <td> Potência: </td> <td><input type=\"text\" name=\"potencia\" /></td>\n" +
+"                </tr>\n" +
+"                <tr>\n" +
+"                    <td> Ano: </td> <td><input type=\"text\" name=\"ano\" /></td>\n" +
+"                </tr>\n" +
+"                <tr> \n" +
+"                    <td> Valor: </td> <td> <input type=\"text\" name=\"valor\" /></td>\n" +
+"                </tr>\n" +
+"            </table>\n" +
+"            <br>\n" +
+"\n" +
+"            <span> <input type=\"submit\" value=\"Cadastrar\" name=\"acao\" style=\"background-color: green\" /> </span>\n" +
+"            <a href=\"index.jsp\"> <input type=\"button\" value=\"Cancelar\" style=\"background-color: red\" /> </a>\n" +
+"        </form> ");
+           
+            out.println("</body>");
+            out.println("</html>");
+
+        }
     }
 
     @Override
