ca773327354349e497734028628649d347674134
adaog <adaog@172.16.1.4>
2017-09-26 15:52:15 -0300

    att javascript

--- web/js/myscripts.js
+++ web/js/myscripts.js
@@ -40,77 +40,91 @@ function maskCPF(CPF) {
     }
 }
 
-function validaCPF(cpf) {
-    erro = new String;
+    function validaCPF() {
+        var cpf = document.getElementById("cpf")
+        erro = new String;
 
-    if (cpf.value.length == 14) {
-        cpf.value = cpf.value.replace('.', '');
-        cpf.value = cpf.value.replace('.', '');
-        cpf.value = cpf.value.replace('-', '');
+        if (cpf.value.length == 14) {
+            cpf.value = cpf.value.replace('.', '');
+            cpf.value = cpf.value.replace('.', '');
+            cpf.value = cpf.value.replace('-', '');
 
-        var nonNumbers = /\D/;
+            var nonNumbers = /\D/;
 
-        if (nonNumbers.test(cpf.value)) {
-            erro = "A verificacao de CPF suporta apenas números!";
-        } else {
-            if (cpf.value == "00000000000" ||
-                    cpf.value == "11111111111" ||
-                    cpf.value == "22222222222" ||
-                    cpf.value == "33333333333" ||
-                    cpf.value == "44444444444" ||
-                    cpf.value == "55555555555" ||
-                    cpf.value == "66666666666" ||
-                    cpf.value == "77777777777" ||
-                    cpf.value == "88888888888" ||
-                    cpf.value == "99999999999") {
-
-                erro = "Número de CPF inválido!"
-            }
-
-            var a = [];
-            var b = new Number;
-            var c = 11;
-
-            for (i = 0; i < 11; i++) {
-                a[i] = cpf.value.charAt(i);
-                if (i < 9)
-                    b += (a[i] * --c);
-            }
-
-            if ((x = b % 11) < 2) {
-                a[9] = 0
+            if (nonNumbers.test(cpf.value)) {
+                erro = "A verificacao de CPF suporta apenas números!";
             } else {
-                a[9] = 11 - x
+                if (cpf.value == "00000000000" ||
+                        cpf.value == "11111111111" ||
+                        cpf.value == "22222222222" ||
+                        cpf.value == "33333333333" ||
+                        cpf.value == "44444444444" ||
+                        cpf.value == "55555555555" ||
+                        cpf.value == "66666666666" ||
+                        cpf.value == "77777777777" ||
+                        cpf.value == "88888888888" ||
+                        cpf.value == "99999999999") {
+
+                    erro = "Número de CPF inválido!"
+                }
+
+                var a = [];
+                var b = new Number;
+                var c = 11;
+
+                for (i = 0; i < 11; i++) {
+                    a[i] = cpf.value.charAt(i);
+                    if (i < 9)
+                        b += (a[i] * --c);
+                }
+
+                if ((x = b % 11) < 2) {
+                    a[9] = 0
+                } else {
+                    a[9] = 11 - x
+                }
+                b = 0;
+                c = 11;
+
+                for (y = 0; y < 10; y++)
+                    b += (a[y] * c--);
+
+                if ((x = b % 11) < 2) {
+                    a[10] = 0;
+                } else {
+                    a[10] = 11 - x;
+                }
+
+                if ((cpf.value.charAt(9) != a[9]) || (cpf.value.charAt(10) != a[10])) {
+                    erro = "Número de CPF inválido.";
+                }
             }
-            b = 0;
-            c = 11;
-
-            for (y = 0; y < 10; y++)
-                b += (a[y] * c--);
-
-            if ((x = b % 11) < 2) {
-                a[10] = 0;
-            } else {
-                a[10] = 11 - x;
-            }
-
-            if ((cpf.value.charAt(9) != a[9]) || (cpf.value.charAt(10) != a[10])) {
+        } else
+        {
+            if (cpf.value.length == 0)
+                return false
+            else
                 erro = "Número de CPF inválido.";
-            }
         }
-    } else
-    {
-        if (cpf.value.length == 0)
-            return false
-        else
-            erro = "Número de CPF inválido.";
+        if (erro.length > 0) {
+            alert(erro);
+            cpf.value = "";
+            cpf.focus();
+            return false;
+        }
+        return true;
     }
-    if (erro.length > 0) {
-        alert(erro);
-        cpf.value = "";
-        cpf.focus();
-        return false;
-    }
-    return true;
-}
 
+
+    function valida_nome() {
+        var filter_nome = /^([a-zA-Zà-úÀ-Ú]|\s+)+$/;
+        var nome = document.getElementById("input_nome")
+        if (!filter_nome.test(nome.value)) {
+            alert("ATENÇÂO! \n Campo Nome não pode conter carácteres especiais!")
+            nome.value = '';
+            nome.focus();
+            return false;
+        }
+        return true;
+    }
+    
